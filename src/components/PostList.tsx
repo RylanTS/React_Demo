48fb3edf611a4a4e860fc7010281bee7f2c54a42
Tristan <strausstristan9@gmaul.com>
2024-09-05 16:02:42 +0200

    Added card and grid but still has issue with grid staying to the left

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -3,21 +3,67 @@ import { PostData } from "../interfaces";
 import { editablePostAtom, postsAtom } from "../atoms/atoms";
 import { useAtomValue } from "jotai";
 import Post from "./Post";
+import {
+    Box,
+    Paper,
+    Grid,
+    Grid2,
+    styled,
+    experimentalStyled
+} from '@mui/material'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
 export default function PostList() {
 
     const posts: PostData[] = useAtomValue<PostData[]>(postsAtom);
 
     return (
-        <>
-            {posts.length > 0 ? <ul className={"posts"}>
+        <Box>
+            {posts.length > 0 ? <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            >
+            <Grid size={6}>
+                <Item>
                 {posts.map((post: PostData, index) => <Post key={index + post.author + post.body} post={post}/>)}
-            </ul> : null}
+                </Item>
+            </Grid>
+            </Grid> : null}
             {posts?.length === 0 ?
             <div style={{textAlign: 'center', color: 'white'}}>
             <h2>There are no posts yet.</h2>
             <p>Start adding some!</p>
             </div> : null}
-        </>
+        </Box>
     );
 }
+
+// export default function RowAndColumnSpacing() {
+//   return (
+//     <Box sx={{ width: '100%' }}>
+//       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+//         <Grid size={6}>
+//           <Item>Post Test</Item>
+//         </Grid>
+//         <Grid size={6}>
+//           <Item>Post Test</Item>
+//         </Grid>
+//         <Grid size={6}>
+//           <Item>Post Test</Item>
+//         </Grid>
+//         <Grid size={6}>
+//           <Item>Post Test</Item>
+//         </Grid>
+//       </Grid>
+//     </Box>
+//   );
+// }
+
